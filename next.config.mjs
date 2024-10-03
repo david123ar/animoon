4ec7ed8cdcca97aeda3d30c765f74eca29cb277a
rishabh-star <risingmerta@gmail.com>
2024-10-03 17:10:12 +0530

    fitst commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint during the build process
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Disable TypeScript errors during the build process
+  },
+};
 
 export default nextConfig;
