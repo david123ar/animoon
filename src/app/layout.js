3831336689db1759e2691b4daaf9b497dc33e551
rishabh-star <risingmerta@gmail.com>
2024-10-03 18:09:50 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,18 @@
 // app/layout.jsx
-import './globals.css';
+import "./globals.css";
 
 export const metadata = {
-  title: 'Animoon | Free Anime Streaming | Watch Anime Online',
-  description: 'Watch free anime online in HD at Animoon.me. Stream the latest anime episodes with Kaido or Kaido.to and explore anime like Hianime.',
-  keywords: 'free anime, anime, watch anime, anime streaming, animoon, kaido, kaido.to, hianime, watch anime online',
+  title: "Animoon | Free Anime Streaming | Watch Anime Online",
+  description:
+    "Watch free anime online in HD at Animoon.me. Stream the latest anime episodes with Kaido or Kaido.to and explore anime like Hianime.",
+  keywords:
+    "free anime, anime, watch anime, anime streaming, animoon, kaido, kaido.to, hianime, watch anime online",
   robots: {
     index: true,
     follow: true,
+  },
+  verification: {
+    google: "x0aiWAODNGU-1UA2FXyORfyme9uWJir7mIMu8AMmLm4",
   },
 };
 
